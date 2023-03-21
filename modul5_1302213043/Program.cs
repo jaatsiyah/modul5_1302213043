705a9de0b6ef04629f783c985ff311f6dda80cb8
unknown <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-21 17:02:53 +0700

    menambahkan class SimpleDataBase

--- modul5_1302213043/Program.cs
+++ modul5_1302213043/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace modul5_1302213043
+﻿public class SimpleDataBase<T>
 {
-    internal class Program
+    private List<T> storedData;
+
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
     {
-        static void Main(string[] args)
+        inputDates= new List<DateTime>();   
+        storedData = new List<T>();
+    }
+
+    public void AddNewdata(T storedData)
+    {
+        this.storedData.Add(storedData);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllaData()
+    {
+        for(int i = 0; i < storedData.Count; i++)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Data " + (i+1) + " berisi : " + storedData[i] + 
+                ", yang disimpan pada waktu UTC: " + inputDates[i].ToString());
         }
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        SimpleDataBase<int> storedData = new SimpleDataBase<int>();
+        storedData.AddNewdata(12);
+        storedData.AddNewdata(34);
+        storedData.AddNewdata(56);
+
+        storedData.PrintAllaData();
     }
 }
