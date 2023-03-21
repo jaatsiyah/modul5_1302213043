e780e6656d2ca28f2c0b096fdd543820547edd75
unknown <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-21 16:21:28 +0700

    “menambahkan class Penjumlahan dengan method  JumlahTigaAngka

--- modul5_1302213043/Program.cs
+++ modul5_1302213043/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace modul5_1302213043
+﻿using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+
+public class Penjumlahan
 {
-    internal class Program
+    public static T JumlahTigaAngka<T>(T num1,T num2,T num3)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        dynamic sum = num1;
+        dynamic sum2 = num2;
+        dynamic sum3 = num3;
+
+        return (sum + sum2 + sum3) ;
+
+        
+    }
+}
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan penjumlahan= new Penjumlahan();
+        double num1 = 13;
+        double num2 = 22;
+        double num3 = 13;
+
+        Penjumlahan.JumlahTigaAngka(num1,num2,num3);
+
     }
 }
