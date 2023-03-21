72cdc9ba93fe8d20d4e08e63398ffe1f6d7e41c0
unknown <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-21 15:57:46 +0700

    first commit

---
+++ modul5_1302213043/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul5_1302213043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
